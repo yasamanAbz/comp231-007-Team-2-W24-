8cff87d484f2b9d169dbbfacd682dd5043ec9f12
TirthGajjar2002 <141785042+TirthGajjar2002@users.noreply.github.com>
2024-04-11 16:18:29 -0400

    Update SchedulePage.js

--- frontend/src/components/Appointments/SchedulePage.js
+++ frontend/src/components/Appointments/SchedulePage.js
@@ -1,31 +1,13 @@
-// SchedulePage.js
-import React, { useState } from 'react';
-import AppointmentForm from './AppointmentForm';
-import AppointmentList from './AppointmentList';
-import AppointmentActions from './AppointmentActions';
+import React from 'react';
+import AppointmentScheduler from './AppointmentScheduler';
 
-function SchedulePage() {
-  const [appointments, setAppointments] = useState([]);
-
-  const handleSchedule = (newAppointment) => {
-    setAppointments([...appointments, newAppointment]);
-  };
-
-  const handleCancel = () => {
-    // Logic to cancel appointment
-  };
-
-  const handleReschedule = () => {
-    // Logic to reschedule appointment
-  };
-
+const SchedulePage = () => {
   return (
     <div>
-      <AppointmentForm onSchedule={handleSchedule} />
-      <AppointmentList appointments={appointments} />
-      <AppointmentActions onCancel={handleCancel} onReschedule={handleReschedule} />
+      <h1>Schedule an Appointment</h1>
+      <AppointmentScheduler />
     </div>
   );
-}
+};
 
 export default SchedulePage;
