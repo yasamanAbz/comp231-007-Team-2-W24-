624bd46f2bdcaa29db07766afdf09bd4aa4a57d5
TirthGajjar2002 <141785042+TirthGajjar2002@users.noreply.github.com>
2024-04-11 16:25:35 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import "dotenv/config";
-import livekitRoutes from "./routes/livekitRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
+const express = require('express');
+const mysql = require('mysql');
+const appointmentsRoutes = require('./routes/appointmentsRoutes');
+
 const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '8911',
+  database: 'appointment'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
+
 app.use(express.json());
+app.use('/api', appointmentsRoutes);
 
-app.use(cors());
-app.use("/livekit", livekitRoutes);
-app.use("/api/auth", authRoutes);
-app.get("/", (req, res) => {
-  res.send("<h1>Hello, Express.js Server!</h1>");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
-// Specify the port to listen on
-const port = process.env.PORT || 8081; // You can use environment variables for port configuration
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Could not connect to MongoDB:", err));
